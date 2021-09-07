adea7d13d9d7a6749e826f2a21ac7c166c1f6015
Mario Todaro <mariotodaro@MBP-di-Mario.station>
2021-09-07 16:30:40 +0200

    make it better

--- db/config.js
+++ db/config.js
@@ -1,10 +1,9 @@
 exports.config = {
     connectionLimit: 10,
-    host: 'localhost',
+    host: 'eu-cdbr-west-01.cleardb.com',
     // Non usiamo *** mai *** root senza password
-    user: 'root',
-    password: '',
-    database: 'stardust',
+    user: 'b95dc8c1785b4d',
+    password: 'c3baea9b',
+    database: 'heroku_8729383960508c1',
     multipleStatements: true // consente query multiple in un'unica istruzione SQL
 }
-
